2d9f6a289a01f0a1785547efceebae83a8225c0c
idahammer <ida.hammer@gmail.com>
2016-01-26 17:14:30 +0100

    test

--- assignment_1/src/assignment_1/main.java
+++ assignment_1/src/assignment_1/main.java
@@ -31,11 +31,12 @@ import java.awt.Insets;
 public class main extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField navnredi;
+	private JTextField navnredi2;
+	private JTextField navnredi3;
+	private JTextField navnredi4;
+	private JTextField navnredi5;
 	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
 
 	/**
 	 * Launch the application.
@@ -88,11 +89,13 @@ public class main extends JFrame {
 		contentPane.add(btnUdskriv);
 		
 		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBackground(Color.LIGHT_GRAY);
 		menuBar.setMargin(new Insets(1, 1, 1, 1));
 		menuBar.setBounds(0, 0, 132, 22);
 		contentPane.add(menuBar);
 		
-		JMenu mnMedlemmer = new JMenu("Medlemmer");
+		JMenu mnMedlemmer = new JMenu("Menu");
+		mnMedlemmer.setBackground(Color.LIGHT_GRAY);
 		menuBar.add(mnMedlemmer);
 		
 		JMenu mnNyeMedlemmer = new JMenu("Nye medlemmer");
@@ -107,70 +110,90 @@ public class main extends JFrame {
 		JMenuItem mntmRedigr = new JMenuItem("Redigér");
 		mnEksisterendeMedlemmer.add(mntmRedigr);
 		
+		JMenu mnHjlp = new JMenu("Hjælp");
+		mnMedlemmer.add(mnHjlp);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Hjælp");
+		mnHjlp.add(mntmNewMenuItem);
+		
+		JMenuItem mntmOmProgrammet = new JMenuItem("Om Programmet");
+		mnHjlp.add(mntmOmProgrammet);
+		
 		JLabel lblNewLabel = new JLabel("Navn:");
 		lblNewLabel.setBackground(Color.WHITE);
 		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setBounds(123, 121, 61, 16);
+		lblNewLabel.setBounds(123, 87, 61, 16);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Personnr:");
 		lblNewLabel_1.setForeground(Color.WHITE);
-		lblNewLabel_1.setBounds(123, 149, 61, 16);
+		lblNewLabel_1.setBounds(123, 118, 61, 16);
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("Adresse:");
 		lblNewLabel_2.setForeground(Color.WHITE);
-		lblNewLabel_2.setBounds(123, 177, 61, 16);
+		lblNewLabel_2.setBounds(123, 149, 61, 16);
 		contentPane.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Telf:");
 		lblNewLabel_3.setForeground(Color.WHITE);
-		lblNewLabel_3.setBounds(123, 205, 61, 16);
+		lblNewLabel_3.setBounds(123, 177, 61, 16);
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("E-mail:");
 		lblNewLabel_4.setForeground(Color.WHITE);
-		lblNewLabel_4.setBounds(123, 233, 61, 16);
+		lblNewLabel_4.setBounds(123, 205, 61, 16);
 		contentPane.add(lblNewLabel_4);
 		
-		textField = new JTextField();
-		textField.setBounds(187, 116, 130, 26);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		navnredi = new JTextField();
+		navnredi.setBounds(187, 82, 130, 26);
+		contentPane.add(navnredi);
+		navnredi.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(187, 144, 130, 26);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		navnredi2 = new JTextField();
+		navnredi2.setBounds(187, 115, 130, 22);
+		contentPane.add(navnredi2);
+		navnredi2.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(187, 172, 130, 26);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		navnredi3 = new JTextField();
+		navnredi3.setBounds(187, 144, 130, 26);
+		contentPane.add(navnredi3);
+		navnredi3.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(187, 200, 130, 26);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		navnredi4 = new JTextField();
+		navnredi4.setBounds(187, 172, 130, 26);
+		contentPane.add(navnredi4);
+		navnredi4.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(187, 228, 130, 26);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
+		navnredi5 = new JTextField();
+		navnredi5.setBounds(187, 200, 130, 26);
+		contentPane.add(navnredi5);
+		navnredi5.setColumns(10);
 		
 		JButton btnGemndringer = new JButton("Gem Ændringer");
 		btnGemndringer.setBounds(314, 243, 132, 29);
 		contentPane.add(btnGemndringer);
 		
 		JLabel lblNewLabel_5 = new JLabel("New label");
-		lblNewLabel_5.setBounds(348, 89, 96, 137);
+		lblNewLabel_5.setIcon(new ImageIcon("/Users/idahammeringerslev/Desktop/AF5129_ida_h/assignment_1/img/body1.jpg"));
+		lblNewLabel_5.setBounds(322, 85, 124, 154);
 		contentPane.add(lblNewLabel_5);
 		
 		JLabel lblFatFitness = new JLabel("Fat 2 Fitness");
-		lblFatFitness.setFont(new Font("Modern No. 20", Font.PLAIN, 40));
+		lblFatFitness.setFont(new Font("Modern No. 20", Font.PLAIN, 45));
 		lblFatFitness.setForeground(Color.WHITE);
-		lblFatFitness.setBounds(227, 6, 219, 44);
+		lblFatFitness.setBounds(157, 16, 268, 44);
 		contentPane.add(lblFatFitness);
+		
+		textField = new JTextField();
+		textField.setBounds(187, 226, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblBillede = new JLabel("Billede:");
+		lblBillede.setForeground(Color.WHITE);
+		lblBillede.setBounds(123, 233, 61, 16);
+		contentPane.add(lblBillede);
 	}
 	private static void addPopup(Component component, final JPopupMenu popup) {
 	}
